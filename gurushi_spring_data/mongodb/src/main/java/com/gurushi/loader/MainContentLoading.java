f766d1bd13e187589ab018f94d07f84b8abdcca7
Nilesh Vaghela <vaghelan@gmail.com>
2013-06-04 21:53:02 -0700

    Add Loading of Bhagavad Gita Dot Org

--- gurushi_spring_data/mongodb/src/main/java/com/gurushi/loader/MainContentLoading.java
+++ gurushi_spring_data/mongodb/src/main/java/com/gurushi/loader/MainContentLoading.java
@@ -5,17 +5,19 @@ import com.gurushi.data.Scripture;
 
 public class MainContentLoading {
 	public static void main(String[] args) {
-		//File currentDir = new File("F:/Dropbox/www.bhagavad-gita.org/Gita"); // current directory
+		String currentDir = "F:/Dropbox/www.bhagavad-gita.org/Gita"; // current directory
 		//File currentDir = new File("/Users/nileshvaghela/Dropbox/www.bhagavad-gita.org/Gita"); // current directory
-		//BhagavadGitaDotOrg bgo = new BhagavadGitaDotOrg(currentDir);
+		
 		//File currentDir = new File("F:/Dropbox/vedabase.net/bg"); // current directory
-		String currentDir = "/Users/nileshvaghela/Dropbox/vedabase.net/bg";
+		//String currentDir = "/Users/nileshvaghela/Dropbox/vedabase.net/bg";
 		
 		Scripture gita = new Scripture("Bhagavad Gita");
 		gita.setDescription("Lord Krishna clears the doubts of Arjuna in the middle of a battlefield.");
 		
-		VedaBaseDotNetGita vbdn = new VedaBaseDotNetGita(currentDir, gita);
-		vbdn.loadData();
+		//VedaBaseDotNetGita vbdn = new VedaBaseDotNetGita(currentDir, gita);
+		//vbdn.loadData();
+		BhagavadGitaDotOrg bgo = new BhagavadGitaDotOrg(currentDir, gita);
+		bgo.loadData();
 	}
 }
 
