69a83f6095726424b6e7b3d394cc5dfa8b7b9673
Tirthankar Bhowmick <tirthb@gmail.com>
2014-07-06 18:29:33 -0700

    removing @Transactional from TestDaoImpl

--- common-postgres/src/test/com/gurushi/dao/impl/TestDaoImpl.java
+++ common-postgres/src/test/com/gurushi/dao/impl/TestDaoImpl.java
@@ -6,8 +6,6 @@ import javax.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class TestDaoImpl {
@@ -18,7 +16,6 @@ public class TestDaoImpl {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Transactional(propagation=Propagation.MANDATORY)
     public void cleanupTables() {
 		
 		//delete all tables
