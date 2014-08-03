d6203e6822872fc6187e2281fcae67f11a448ddd
Tirthankar Bhowmick <tirthb@gmail.com>
2014-08-02 23:50:33 -0700

    after running vedabase and bhagavad gita org, test is pending

---
+++ sql/gurushi.sql
@@ -0,0 +1,20 @@
+﻿select * from scripture;
+select * from chapter;
+select * from verse_line;
+select * from author;
+select * from commentary;
+
+select 
+c.title chapter_title, 
+c.number chapter_number, 
+v.number_range verse_number,
+vl.line,
+t.text,
+co.text
+from verse v
+inner join chapter c on v.chapter_id = c.id
+inner join verse_line vl on vl.verse_id = v.id
+inner join translation t on t.verse_id = v.id
+inner join commentary co on co.verse_id = v.id and co.text = ''
+order by c.sort_order, v.sort_order, vl.sort_order;
+
